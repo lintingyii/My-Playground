f3cd0786a15b2d2bbefeab065abee887ed0223cb
林庭奕 <morgan.futurenest@gmail.com>
2025-03-09 19:47:02 +0800

    embed-title

--- src/FigmaEmbed.js
+++ src/FigmaEmbed.js
@@ -4,6 +4,7 @@ function FigmaEmbedValentine() {
     return (
       <iframe
         className="figma-embed"
+        title="Valentine's Day"
         src="https://embed.figma.com/proto/bYEMRQBH12g24WhNx0ffy4/2025-Valentine's-day?page-id=0%3A1&node-id=1-72&p=f&viewport=33%2C262%2C0.06&scaling=scale-down&content-scaling=fixed&starting-point-node-id=1%3A2&embed-host=share"
         allowFullScreen
       />
@@ -14,6 +15,7 @@ function FigmaEmbedValentine() {
     return (
       <iframe
         className="figma-embed"
+        title="Christmas"
         src="https://embed.figma.com/proto/PVikhGOizKCPodLiURbeJE/2024-Christmas?page-id=0%3A1&node-id=1-3564&p=f&viewport=-365%2C258%2C0.46&scaling=scale-down&content-scaling=fixed&starting-point-node-id=1%3A202&embed-host=share"
         allowFullScreen
       />
@@ -24,6 +26,7 @@ function FigmaEmbedValentine() {
     return (
       <iframe
         className="figma-embed"
+        title="New Year"
         src="https://embed.figma.com/proto/lqGiCfYhlokpceIFzRgszd/2025-Chinese-New-Year?page-id=0%3A1&node-id=1-1255&viewport=123%2C171%2C0.05&scaling=scale-down-width&content-scaling=fixed&starting-point-node-id=1%3A1255&embed-host=share"
         allowFullScreen
       />
